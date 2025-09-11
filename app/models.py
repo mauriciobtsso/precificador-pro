21e0f64b00f078cb82c5392562cabcba381da02a
m4tatica <comercial@m4tatica.com.br>
2025-09-11 11:20:05 -0300

    FIX: Versão consolidada com correções de formulário e modelo de dados

--- app/models.py
+++ app/models.py
@@ -9,27 +9,18 @@ class Produto(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     codigo = db.Column(db.String(80), unique=True, nullable=False)
     nome = db.Column(db.String(120), nullable=False)
-    
-    # Campos de Custo
     valor_fornecedor_real = db.Column(db.Float, nullable=False, default=0.0)
     desconto_fornecedor_percentual = db.Column(db.Float, default=0.0)
     frete_real = db.Column(db.Float, default=0.0)
     ipi_valor = db.Column(db.Float, default=0.0)
     ipi_tipo = db.Column(db.String(20), default='fixo')
     difal_percentual = db.Column(db.Float, default=0.0)
-    
-    # Novos campos para salvar o estado da precificação (ESSA ERA A PARTE FALTANTE)
     imposto_venda_percentual = db.Column(db.Float, default=0.0)
     metodo_precificacao = db.Column(db.String(20), default='margem')
     valor_metodo = db.Column(db.Float, default=0.0)
-    
-    # Campos de Resultado
     custo_total = db.Column(db.Float, nullable=True)
     preco_a_vista = db.Column(db.Float, nullable=True)
     lucro_liquido_real = db.Column(db.Float, nullable=True)
-
-    def __repr__(self):
-        return f'<Produto {self.nome}>'
 
 class TaxaPagamento(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -37,12 +28,6 @@ class TaxaPagamento(db.Model):
     taxa_percentual = db.Column(db.Float, nullable=False)
     coeficiente = db.Column(db.Float, nullable=False)
 
-    def __repr__(self):
-        return f'<TaxaPagamento {self.metodo}>'
-
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
-
-    def __repr__(self):
-        return f'<User {self.username}>'
